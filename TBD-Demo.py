912e10de50e6ef69f2cb16a03a3bacd9af2c12ee
Muhammadjunaid7864 <muhammadjunai104developer@gmail.com>
2024-09-10 20:54:57 +0500

    Can Save Name

--- TBD-Demo.py
+++ TBD-Demo.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def test():
-    return render_template("index.html")
+def index():
+    return render_template("name_form.html")
+
+@app.route("/submit_name", methods=["POST"])
+def submit_name():
+    name = request.form.get("name")
+    return f"<h1>Hello, {name}!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
